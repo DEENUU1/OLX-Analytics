eb0b91c523f30e22748489f17b5014ea77f1a9c3
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-10 17:52:21 +0200

    Parser | parsing all images from object |
    Using a simple for loop the script can get and return all images for a specified object

--- data/parser.py
+++ data/parser.py
@@ -14,6 +14,7 @@ class HouseData:
     title: str
     created_time: str
     params: list
+    photos: list
     city: str
     region: str
 
@@ -21,19 +22,23 @@ class HouseData:
 class ParserHouse(Parser):
     def data_parse(self, data: dict | None):
         objects = []
-        for object in data['data']:
-            url = object['url']
-            title = object['title']
-            created_time = object['created_time']
-            city = object['location']['city']['name']
-            region = object['location']['region']['name']
+        for object in data["data"]:
+            url = object["url"]
+            title = object["title"]
+            created_time = object["created_time"]
+            city = object["location"]["city"]["name"]
+            region = object["location"]["region"]["name"]
+            params = object.get("params", [])
 
-            params = object.get('params', [])
             params_values = []
             for param in params:
-                param_value = param.get('value')
+                param_value = param.get("value")
                 if param_value is not None:
                     params_values.append(param_value)
+
+            photos_url = []
+            for photo in object['photos']:
+                photos_url.append(photo['link'])
 
             objects.append(
                 HouseData(
@@ -41,7 +46,9 @@ class ParserHouse(Parser):
                     title=title,
                     created_time=created_time,
                     params=params_values,
+                    photos=photos_url,
                     city=city,
-                    region=region
-                ))
+                    region=region,
+                )
+            )
         return objects
