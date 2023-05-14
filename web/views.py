3cdf361c29e74761fa090c4bc0c1a312dcd63c3b
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-14 22:47:44 +0200

    Web | Flask base view template and first view |
    Base.html is a template for another templates, it allows to easiely add scripts, styles, display messages and more
    
    results_view is a test view to display information from the API, it has hard coded data to make this process easier

--- web/views.py
+++ web/views.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-
+from data import fetch_data, parser
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
+def results_view():
+    url = fetch_data.UrlBuilderHouse().build_url(
+        limit="40",
+        area_min="25",
+        price_min="600",
+    )
+    x = parser.Parser(fetch_data.FetchData(url).fetch_data())
+    d = x.data_parser()
+
+    return render_template('results.html', data_list=d)
+
