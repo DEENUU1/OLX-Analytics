9cc02c240e8c108fb1d9171b16554b87efd9fd2e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-19 03:06:16 +0200

    Feature | Flask configuration for sqlite and user system |
    Configuration for sqlite connection
    
    Model User which represents users data like (id, email and url)

--- web/__init__.py
+++ web/__init__.py
@@ -2,19 +2,53 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_login import LoginManager, current_user
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
 
 load_dotenv()
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
     csrf = CSRFProtect()
     csrf.init_app(app)
-    app.secret_key = "develop"
+    migrate = Migrate()
+    migrate.init_app(app, db)
 
     from .views import views
 
     app.register_blueprint(views)
+    from .auth import auth
+
+    app.register_blueprint(auth)
+    from .models import User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    @app.context_processor
+    def inject_user():
+        return dict(user=current_user)
 
     return app
+
+
+def create_database(app):
+    if not os.path.exists("similar_movies" + DB_NAME):
+        with app.app_context():
+            db.create_all()
--- web/test/test_views.py
+++ web/test/test_views.py
@@ -6,9 +6,11 @@ from flask import Markup, session
 @pytest.fixture()
 def app():
     app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
 
     yield app
 
@@ -16,6 +18,7 @@ def app():
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
 
 @pytest.fixture()
 def runner(app):
@@ -26,13 +29,16 @@ def test_home_view_get_method_returns_200_status_code(client):
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_search_apartment_view_get_method_returns_200_status_code(client):
     response = client.get("/search_apartment")
     assert response.status_code == 200
 
+
 def test_search_house_view_get_method_returns_200_status_code(client):
     response = client.get("/search_house")
     assert response.status_code == 200
+
 
 def test_result_view_get_method_returns_200_status_code(client):
     response = client.get("/results")
